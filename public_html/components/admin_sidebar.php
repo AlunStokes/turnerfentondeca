30eb1a27ed35b9b404662ec8dcb4ec8527ecf530
Alunsto <alunsto99@gmail.com>
2016-12-04 21:39:21 -0500

    Fixed mouse scrolling on sidebar

--- public_html/components/admin_sidebar.php
+++ public_html/components/admin_sidebar.php
@@ -96,6 +96,25 @@ $(document).ready(function() {
     }
     $("#sidebar-recent-exmas").append(recent_exams_html);
   });
+
+
+  $('.control-sidebar, .control-sidebar-bg').bind('mousewheel DOMMouseScroll', function(e) {
+    var scrollTo = null;
+
+    if (e.type == 'mousewheel') {
+        scrollTo = (e.originalEvent.wheelDelta * -1);
+    }
+    else if (e.type == 'DOMMouseScroll') {
+        scrollTo = 40 * e.originalEvent.detail;
+    }
+
+    if (scrollTo) {
+        e.preventDefault();
+        $(this).scrollTop(scrollTo + $(this).scrollTop());
+    }
+});
+
+
 });
 
 </script>
